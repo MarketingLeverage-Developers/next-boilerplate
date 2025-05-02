6003ff34e7b7722fc9830646746314f94cfa8ed0
dhkim <dhkim@mleverage.co.kr>
2025-05-02 17:43:07 +0900

    feat: next.config.ts 수정 - next.config.ts 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+// next.config.ts
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    reactStrictMode: true,
+    sassOptions: {
+        additionalData: `
+      $primary-color: ${process.env.NEXT_PUBLIC_PRIMARY_COLOR};
+    `,
+    },
 };
 
 export default nextConfig;
