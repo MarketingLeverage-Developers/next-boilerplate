4dd749a24d755309e7daace0edc45cfb18babc19
mleverage-Intern <intern@mleverage.co.kr>
2025-09-24 13:51:17 +0900

    input 수정

--- src/libs/utils/security.ts
+++ src/libs/utils/security.ts
@@ -8,7 +8,8 @@ export const useSafeInput = () => {
             .replace(/'/g, '&#39;');
 
     const normalizePhone = (input: string) => input.replace(/[^0-9]/g, '');
-    const validateText = (input: string) => input.replace(/[^ㄱ-ㅎ가-힣a-zA-Z0-9\s]/g, '');
+    const validateText = (input: string) =>
+        input.replace(/[^가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9\u1100-\u1112\u318D\u119E\u11A2\u2022\u2025\u00B7\uFE55\s]/g, '');
 
     return { sanitize, normalizePhone, validateText };
 };
